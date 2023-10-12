aa82dca9ea8ee676aef888c9e926155673bd8c2c
Pedro <pedro0702lucas@gmail.com>
2023-10-12 16:51:48 -0300

    Created kekkeigenkai page and card

---
+++ src/components/CardKekkeiGenkai/Style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const KekkeiGenkaisCard = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+export const KekkeiGenkaiItem = styled.div`
+    width: 20%; /* 20% para 5 colunas */
+    box-sizing: border-box;
+`;
--- src/pages/Styles/PagesStyle.js
+++ src/pages/Styles/PagesStyle.js
@@ -26,3 +26,12 @@ export const VillagesContainer = styled.div`
     height: 100vh; 
     padding: 20px;
 `;
+
+export const KekkeiGenkaisContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh; 
+    padding: 20px;
+`;
